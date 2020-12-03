d08f4e2bfcce8d2bffb768f013b8112b05563d30
Ana Carrizales <acarrizales2@ucol.mx>
2020-12-03 17:57:44 -0600

    contarImparesDo() was tested an a conditional was added

--- src/main.js
+++ src/main.js
@@ -87,6 +87,13 @@ export default class App {
     contarImparesDo(inicio,fin)
     {
         let impares = 0;
+        if(inicio > fin)
+        {
+            let temporal;
+            temporal = inicio;
+            inicio = fin;
+            fin = temporal;
+        }
         let i = inicio;
         do
         {
@@ -97,6 +104,7 @@ export default class App {
             i++
         }
         while(i<=fin)
+        return impares;
     }
 }
 
@@ -111,9 +119,13 @@ console.log(app.contarImparesFor(20,5));//Resultado debe ser 8
 console.log("Probando sumarParesWhile()");
 console.log(app.sumarParesWhile());//Resultado debe ser 110
 
-console.log("Probando contarImparesWhile()")
+console.log("Probando contarImparesWhile()");
 console.log(app.contarImparesWhile(3,25));//Resultado debe ser 12
 console.log(app.contarImparesWhile(25,3));//Resultado debe ser 12
 
-console.log("Probando SumarParesDo()")
+console.log("Probando sumarParesDo()");
 console.log(app.sumarParesDo());//Resultado debe ser 110
+
+console.log("Probando contarImparesDo()")
+console.log(app.contarImparesDo(6,24));
+console.log(app.contarImparesDo(24,6));
